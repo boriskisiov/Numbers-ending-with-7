70f292c1725f3f4532638d1adb81df52bca91284
boriskisiov <boriskisiov@gmail.com>
2023-10-19 10:10:55 +0300

    Add project files.

---
+++ Numbers ending with 7/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Numbers_ending_with_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           int n = int.Parse(Console.ReadLine());
+            for (int i = 7; i <= n; i += 10)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
